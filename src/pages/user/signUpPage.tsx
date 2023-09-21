7e9e9a779c7e445dde6dab5559ff7a0f3b5f57ce
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2023-09-22 03:53:11 +0900

    feat: [김영진] 회원가입시 ,이메일 패스워드, 약관동의 유효성 검사 추가 [REACT-SHOP-FRONT-22]

--- src/pages/user/signUpPage.tsx
+++ src/pages/user/signUpPage.tsx
@@ -18,11 +18,11 @@ import Container from "@mui/material/Container";
 import { useDispatch } from "react-redux";
 import { signUp } from "../../redux/user/userActions";
 
-import { ThunkDispatch } from 'redux-thunk';
-import { AnyAction } from 'redux';
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "redux";
 import { RootState } from "@/redux/reducers";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
+import { FormHelperText } from "@mui/material";
 
 const Copyright = (props: any) => {
   return (
@@ -47,32 +47,17 @@ const Copyright = (props: any) => {
 
 export const SignUpPage: React.FC = () => {
   const navigate = useNavigate();
+  const dispatch: ThunkDispatch<RootState, unknown, AnyAction> = useDispatch();
 
   const [isChecked, setIsChecked] = useState(false);
 
-  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked(event.target.checked);
-  };
-  const [passwordsMatch, setPasswordsMatch] = React.useState(false);
-  const checkPasswordsMatch = (event: ChangeEvent<HTMLInputElement>) => {
-    // 비밀번호 확인 검증 시뮬레이션
-    // 여기서는 단순히 입력된 두 비밀번호가 같은지 확인
-    const password = event.target.value;
-    // const confirmPassword = document.getElementById("confirmPassword").value;
-    const confirmPasswordElement = document.getElementById(
-      "confirmPassword"
-    ) as HTMLInputElement;
-    const confirmPassword = confirmPasswordElement?.value;
-
-    if (password === confirmPassword) {
-      setPasswordsMatch(true);
-    } else {
-      setPasswordsMatch(false);
-    }
-  };
-
-  // const dispatch = useDispatch();
-  const dispatch: ThunkDispatch<RootState, unknown, AnyAction> = useDispatch();
+  const [emailValid, setEmailValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [confirmValid, setConfirmValid] = useState(false);
+  const [termsAccepted, setTermsAccepted] = useState(false);
+  const [emailError, setEmailError] = useState<string | null>(null);
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [confirmError, setConfirmError] = useState<string | null>(null);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -82,13 +67,55 @@ export const SignUpPage: React.FC = () => {
       password: data.get("password") as string,
       username: data.get("username") as string,
     };
-    console.log("signup-handleSubmit메서드 작동")
-        dispatch(signUp(userData, navigate));
+    console.log("signup-handleSubmit메서드 작동");
+    dispatch(signUp(userData, navigate));
   };
   // console.log({
   //   email: data.get("email"),
   //   password: data.get("password"),
   // });
+
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  const checkEmailValidity = (email: string) => {
+    if (emailRegex.test(email)) {
+      setEmailValid(true);
+      setEmailError(null);
+    } else {
+      setEmailValid(false);
+      setEmailError("이메일 형식에 맞지 않습니다.");
+    }
+  };
+  const passwordRegex =
+    /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+
+  const checkPasswordValidity = (password: string) => {
+    if (passwordRegex.test(password)) {
+      setPasswordValid(true);
+      setPasswordError(null);
+    } else {
+      setPasswordValid(false);
+      setPasswordError(
+        "패스워드는 영어, 숫자, 특수문자로 구성되어야 하며 8자 이상이어야 합니다."
+      );
+    }
+  };
+
+  const checkConfirmValidity = (password: string, confirm: string) => {
+    if (password === confirm) {
+      setConfirmValid(true);
+      setConfirmError(null);
+    } else {
+      setConfirmValid(false);
+      setConfirmError("패스워드가 일치하지 않습니다.");
+    }
+  };
+
+  const handleTermsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("Checkbox changed:", event.target.checked); // 이 로그가 제대로 출력되는지 확인
+    setIsChecked(event.target.checked);
+    setTermsAccepted(event.target.checked);
+  };
 
   return (
     // <ThemeProvider theme={defaultTheme}>
@@ -115,7 +142,7 @@ export const SignUpPage: React.FC = () => {
           sx={{ mt: 3 }}
         >
           <Grid container spacing={2}>
-            <Grid item xs={12}>
+            <Grid item xs={7}>
               <TextField
                 autoComplete="given-name"
                 name="username"
@@ -126,6 +153,19 @@ export const SignUpPage: React.FC = () => {
                 autoFocus
               />
             </Grid>
+            <Grid item xs={5}>
+              <Button
+                type="button"
+                fullWidth
+                variant="contained"
+                color="primary"
+                sx={{ height: 50 }}
+                // TODO: 이메일 인증 함수 추가
+                onClick={() => {}}
+              >
+                Verify Email
+              </Button>
+            </Grid>
 
             <Grid item xs={7}>
               <TextField
@@ -135,7 +175,11 @@ export const SignUpPage: React.FC = () => {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+                onChange={(e) => checkEmailValidity(e.target.value)}
               />
+              {emailError && (
+                <FormHelperText error>{emailError}</FormHelperText>
+              )}
             </Grid>
             <Grid item xs={5}>
               <Button
@@ -159,7 +203,11 @@ export const SignUpPage: React.FC = () => {
                 type="password"
                 id="password"
                 autoComplete="new-password"
+                onChange={(e) => checkPasswordValidity(e.target.value)}
               />
+              {passwordError && (
+                <FormHelperText error>{passwordError}</FormHelperText>
+              )}
             </Grid>
 
             <Grid item xs={12}>
@@ -171,17 +219,24 @@ export const SignUpPage: React.FC = () => {
                 type="password"
                 id="confirmPassword"
                 autoComplete="new-password"
-                // TODO: 비밀번호 확인 검증 함수 추가
-                onChange={checkPasswordsMatch}
+                onChange={(e) => {
+                  checkConfirmValidity(
+                    (document.getElementById("password") as HTMLInputElement)
+                      .value,
+                    e.target.value
+                  );
+                }}
               />
+              {confirmError && (
+                <FormHelperText error>{confirmError}</FormHelperText>
+              )}
             </Grid>
             <Grid item xs={12}>
               <FormControlLabel
                 control={
                   <Checkbox
                     checked={isChecked}
-                    onChange={handleCheckboxChange}
-                    value="allowService"
+                    onChange={handleTermsChange}
                     color="primary"
                   />
                 }
@@ -198,12 +253,16 @@ export const SignUpPage: React.FC = () => {
             fullWidth
             variant="contained"
             sx={{ mt: 3, mb: 2 }}
+            disabled={
+              !(emailValid && passwordValid && confirmValid && termsAccepted)
+            }
           >
             Sign Up
           </Button>
+
           <Grid container justifyContent="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/sign-in" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
